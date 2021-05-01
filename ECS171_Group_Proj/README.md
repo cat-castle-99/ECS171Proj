e5a24e5cff4e6837c2c65cea8803a78b6a37f5e8
cat-castle-99 <56051215+cat-castle-99@users.noreply.github.com>
2021-04-30 23:22:16 -0700

    Add files via upload

---
+++ ECS171_Group_Proj/README.md
@@ -0,0 +1,25 @@
+# README.md
+
+## Files:
+.
+├── static
+│   ├── reset.css                 # Overrides browser-default styling so our website will look the same regardless of browser
+│   └── style.css                 # Provides style information
+│
+├── templates
+│   └── index.html                # The static web file to be served
+├── server.py                     # The Controller of our webapp-will serve the index.html page, as well as any Model files
+└── README.md                   # This file
+
+
+
+## Current Functionality:
+- Can receive user input--both a categorical entry and a numerical entry  
+    - As yet, not specific to the features in our dataset
+- Can return information from server.py to client-side(View)
+
+## Todo:
+- Make user input areas feature-specific
+- Integrate with model  
+    - parse input and pass to model file
+    - read output and format so it can be passed back to client-side/View
---
+++ ECS171_Group_Proj/server.py
@@ -0,0 +1,44 @@
+#server. py
+#This is our controller file.
+#It's responsibilities are:
+    #Handling the relationship between the View files and the Model files
+    #Passing input to the View to the Model
+    #Passing output from the Model to the View
+from flask import Flask, request, render_template, jsonify
+
+app = Flask(__name__)
+
+
+#Upper just capitalizes the message
+def do_something(text1,text2):
+   text1 = text1.upper()
+   combine = text1 + text2
+   return combine
+
+#The app.route references the url referenced, yes AJAX requests are using the url to make the HTTP request
+#this means that everytime you type a url you're also making an HTTP request
+#idk if this is too much detail
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/getModelOutput', methods=['GET','POST'])
+#any reference to "form" is referencing the JSON we passed in the post request
+#on that note, the request referenced here is a data structure that contains relevant information sent with the HTTP request, including our JSON
+#therefore: request.form = the JSON we sent
+def my_form_post():
+    text1 = request.form['categ1']
+    text2 = request.form['numer1']
+    combine = do_something(text1,text2)
+    #Data is sent to and from the sides as a JSON. In this example, it's just sending the combined form of our input back to the client side
+    #In the future, we'll replace that do_someething() function with a function that parses the JSON for the model
+    #Then, after running the model, we'll put the output from the model into a new JSON that we'll send back to the client-side
+    result = {
+        "output": combine
+    }
+    result = {str(key): value for key, value in result.items()}
+    return jsonify(result=result)
+
+if __name__ == '__main__':
+    app.run(debug=True)
