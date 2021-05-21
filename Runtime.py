fa298f9bf6baefed4cda03e4973d8e4efc84b29c
sperelgut <43866847+sperelgut@users.noreply.github.com>
2021-05-21 00:42:41 -0700

    Update Runtime.py
    added new variable datasetRaw to represent raw dataset
    add new changes to the prediction
    began work on graphing

--- Runtime.py
+++ Runtime.py
@@ -8,22 +8,27 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import precision_score, recall_score
 from sklearn.metrics import mean_squared_error
 from sklearn.preprocessing import StandardScaler
-
+from sklearn.preprocessing import MinMaxScaler
 
 
 
 class logModel:
     def __init__(self):
-        self.seed=0
-        self.__dataSet=None
-        self.__model=None
-        self.__userInput=[]
+        self.seed=0 #seed for train test split. still needs to be implemented
+        self.__dataSetRaw=None #raw unnormalizzed dataset
+        self.__dataSet = None #nomarlized dataset
+        self.__model=None # fitted model from pickles
+        self.__userInput={} #users input format: dictionary
+        self.__userOutput=0 #users result
+
+    def prepare(self):
+        return 0
 
     def readModel(self,filename):
         #read in the model
         try:
             loaded_model = pickle.load(open(filename, 'rb'))
-            self.__dataSet=loaded_model
+            self.__model=loaded_model
             return 0
         except:
             print("Model Read ERROR")
@@ -32,29 +37,40 @@ class logModel:
     def readDataSet(self,filename):
         #read in the dataset
         try:
-            self.__model = pd.read_csv(filename)
+            self.__dataSetRaw = pd.read_csv(filename)
             return 0
         except:
             print("Dataset Read ERROR")
             return -1
 
-    def makePrediction(self, userinput=[]):
-        self.__userInput=userinput
+    def makePrediction(self, userinput=None):
+        self.__dataSetRaw = self.__dataSetRaw.append(userinput, ignore_index=True)
+        scale=MinMaxScaler()
+        self.__dataSet = scale.fit_transform(self.__dataSetRaw)
+        self.__userInput=self.__dataSet.iloc[-1:]
         try:
-            return  self.__dataSet.predict([userinput])
+            predict = self.__model.predict([self.__userInput])
+            self.__userOutput=predict
+            return predict
         except:
             print("Prediction ERROR")
             return -1
 
     def generateGraph(self):
+        # FIXME this function is far from finished
         try:
+            #generate graph then write it
+            self.writegraph(plt)
             return 0
         except:
             print("Graph generation ERROR")
             return -1
 
-    def writeGraphs(self):
+    def writeGraphs(self,plt,key):
+        #FIXME this function is far from finished
         try:
+            filename="Graph"+"_"+key+".png"
+            plt.savefig(filename, format="PNG")
             return 0
         except:
             print("Graph write ERROR")
@@ -67,5 +83,4 @@ myModel.readDataSet("data.csv")
 myModel.readModel("model.file")
 myModel.makePrediction()
 myModel.generateGraph()
-myModel.writeGraphs()
 
